514754f90c2a73fdef5d01441b2c52276af3bc14
h0008 <duy6902@gmail.com>
2024-01-31 16:08:56 +0700

    gggg

--- ASM1/Program.cs
+++ ASM1/Program.cs
@@ -7,25 +7,27 @@ void WaterBill()
 {
     Console.Write("Enter customer's name: ");
     string customer = Convert.ToString(Console.ReadLine());
-    Console.WriteLine("Please choose the type of customer.");
-    Console.WriteLine("Enter 1 if you are Household customer.");
-    Console.WriteLine("Enter 2 if you are Administrative agency, public services.");
-    Console.WriteLine("Enter 3 if you are Production units.");
-    Console.WriteLine("Enter 4 if you are Business services.");
+    Console.WriteLine("Enter 1 if you are a household customer.");
+    Console.WriteLine("Enter 2 if you are an administrative agency or public services.");
+    Console.WriteLine("Enter 3 if you are a production unit.");
+    Console.WriteLine("Enter 4 if you are a business service.");
+    int milliseconds = 1000;
+    Thread.Sleep(milliseconds);
+    Console.Write("Please select the type of customer: ");
     int typeCustomer = Convert.ToInt32(Console.ReadLine());
     if (typeCustomer == 1)
     {
         //Household customer
         //Required number of family members
-        Console.WriteLine("Enter number of family members: ");
+        Console.Write("Enter number of family members: ");
         int numberMember = Convert.ToInt32(Console.ReadLine());
         if (numberMember >= 1)
         {
-            Console.WriteLine("Enter last month's water number: ");
+            Console.Write("Enter last month's water number: ");
             int waterNumberLastMonth = Convert.ToInt32(Console.ReadLine());
             if (waterNumberLastMonth >= 0)
             {
-                Console.WriteLine("Enter this month's water number: ");
+                Console.Write("Enter this month's water number: ");
                 int waterNumberCurrentMonth = Convert.ToInt32(Console.ReadLine());
                 if (waterNumberLastMonth <= waterNumberCurrentMonth)
                 {
@@ -36,6 +38,10 @@ void WaterBill()
                     {
                         money = waterNumber * 5973 * 1.1;
                     }
+                    else if (waterNumberPeople > 10 && waterNumberPeople <= 20)
+                    {
+                        money = waterNumber * 7052 * 1.1;
+                    }
                     else if (waterNumberPeople > 20 && waterNumberPeople <= 30)
                     {
                         money = waterNumber * 8699 * 1.1;
@@ -44,11 +50,11 @@ void WaterBill()
                     {
                         money = waterNumber * 15929 * 1.1;
                     }
-                    Console.WriteLine("Your family's water bill is: {0} dong", money);
+                    Console.WriteLine("Your family's water bill is: {0} VND.", money);
                 }
                 else
                 {
-                    Console.WriteLine("Last month's water number can't be bigger than this month's");
+                    Console.WriteLine("Last month's water number can't be bigger than this month's.");
                 }
             }
             else
@@ -58,28 +64,28 @@ void WaterBill()
         }
         else
         {
-            Console.WriteLine("Family member can't be smaller than 1");
+            Console.WriteLine("Family member can't be smaller than 1.");
         }
     }
     else if (typeCustomer == 2)
     {
         //Administrative agency, public services
-        Console.WriteLine("Enter last month's water number: ");
+        Console.Write("Enter last month's water number: ");
         int waterNumberLastMonth = Convert.ToInt32(Console.ReadLine());
         if (waterNumberLastMonth >= 0)
         {
-            Console.WriteLine("Enter this month's water number: ");
+            Console.Write("Enter this month's water number: ");
             int waterNumberCurrentMonth = Convert.ToInt32(Console.ReadLine());
             if (waterNumberLastMonth <= waterNumberCurrentMonth)
             {
                 int waterNumber = waterNumberCurrentMonth - waterNumberLastMonth;
                 double money = 0;
                 money = waterNumber * 9955 * 1.1;
-                Console.WriteLine("Your administrative agency or public service's water bill is: {0} dong", money);
+                Console.WriteLine("Your administrative agency or public service's water bill is: {0} VND", money);
             }
             else
             {
-                Console.WriteLine("Last month's water number can't be bigger than this month's");
+                Console.WriteLine("Last month's water number can't be bigger than this month's.");
             }
         }
         else
@@ -90,7 +96,7 @@ void WaterBill()
     else if (typeCustomer == 3)
     {
         //Production units
-        Console.WriteLine("Enter last month's water number: ");
+        Console.Write("Enter last month's water number: ");
         int waterNumberLastMonth = Convert.ToInt32(Console.ReadLine());
         if (waterNumberLastMonth >= 0)
         {
@@ -101,11 +107,11 @@ void WaterBill()
                 int waterNumber = waterNumberCurrentMonth - waterNumberLastMonth;
                 double money = 0;
                 money = waterNumber * 11615 * 1.1;
-                Console.WriteLine("Your production unit's water bill is: {0} dong", money);
+                Console.WriteLine("Your production unit's water bill is: {0} VND", money);
             }
             else
             {
-                Console.WriteLine("Last month's water number can't be bigger than this month's");
+                Console.WriteLine("Last month's water number can't be bigger than this month's.");
             }
         }
         else
@@ -116,7 +122,7 @@ void WaterBill()
     else if (typeCustomer == 4)
     {
     //Business services
-    Console.WriteLine("Enter last month's water number: ");
+    Console.Write("Enter last month's water number: ");
     int waterNumberLastMonth = Convert.ToInt32(Console.ReadLine());
     if (waterNumberLastMonth >= 0)
     {
@@ -127,11 +133,11 @@ void WaterBill()
             int waterNumber = waterNumberCurrentMonth - waterNumberLastMonth;
             double money = 0;
             money = waterNumber * 22068 * 1.1;
-            Console.WriteLine("Your business service's water bill is: {0} dong", money);
+            Console.WriteLine("Your business service's water bill is: {0} VND", money);
         }
         else
         {
-            Console.WriteLine("Last month's water number can't be bigger than this month's");
+            Console.WriteLine("Last month's water number can't be bigger than this month's.");
         }
     }
     else
--- ASM1/obj/Debug/net8.0/ASM1.AssemblyInfo.cs
+++ ASM1/obj/Debug/net8.0/ASM1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASM1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d287bda07ec1774928566589c6b6f3f0a5dcf0df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASM1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASM1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
